485f6dbe52ea4d722dad11f1501edc82757c61c2
Enock MUKOKO MAYA <emukoko@planetworkint.com>
2024-04-01 13:17:33 +0200

    Ajout de la fonctionnalité traduction dans golden master et ajout de traduction de scores names dans la classe TennisGame6

--- python/explication.markdown
+++ python/explication.markdown
@@ -1,6 +1,6 @@
-# Journal de bord Tennis Version 6
+# Journal de bord Tennis Version 6 python
 
-
+le 18/03/2024
 La fonction score de la version 6 est beaucoup trop longue et complexe, on a donc choisi de faire une modification majeure sur cette fonction ainsi que quelques détails mineurs sur d’autres parties du code.
 Score(self) : Dans le code de base, la fonction score utilisait des instructions if, elif et else pour déterminer le type de score. Dans le code refactorisé, cette logique est déplacée sur d’autres fonctions pour améliorer la lisibilité et la maintenabilité. La nouvelle logique d’implémentation est l’ajout des fonctions _tie_score(self), _end_game_score(self) et regular_score(self) pour obtenir le score en fonction de la situation dans la partie en cours.
 _tie_score(self) : Cette fonction gère le cas où les scores des deux joueurs sont égaux. Elle utilise la liste SCORE_NAMES pour obtenir les noms des scores éliminant ainsi la duplication dans le code
@@ -10,3 +10,11 @@ _regular_score(self) : Cette fonction gère les cas où les scores des joueurs s
 
 
 D’autres changements mineurs ont été apportés tels que des espaces autour de ‘==’ ou des accolades qui n’étaient pas nécessaires dans certaines conditions if.
+
+
+# golden master
+le 19/03/2024
+Ajout de golden master dans le projet 
+le 01/04/2024
+Ajout de la traduction de la fonctionnalité traduction dans  golden master
+Et mise à jour de la classe TennisGame6 afin de s'adapter à la langue choisie dans golden master
--- python/tennis6.py
+++ python/tennis6.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 
 class TennisGame6:
-    SCORE_NAMES = ["Love", "Fifteen", "Thirty", "Forty"]  # Utilisation d'une liste SCORE_NAMES pour stocker les noms des scores, ce qui élimine la duplication de code.
-
-    def init(self, player1Name, player2Name):
+    SCORE_NAMES = {
+        "Love": {"en": "Love", "fr": "Zéro"},
+        "Fifteen": {"en": "Fifteen", "fr": "Quinze"},
+        "Thirty": {"en": "Thirty", "fr": "Trente"},
+        "Forty": {"en": "Forty", "fr": "Quarante"},
+        "Deuce": {"en": "Deuce", "fr": "Égalité"},
+        "All": {"en": "All", "fr": "Partout"},
+        "Advantage": {"en": "Advantage", "fr": "Avantage"},
+        "Win for": {"en": "Win for ", "fr": "Victoire pour "}
+    }
+    def __init__(self, player1Name, player2Name,lang):
         self.player1Name = player1Name
         self.player2Name = player2Name
         self.player1Score = 0
         self.player2Score = 0
+        self.lang = lang  # Nouveau: Choix de la langue
+
+    def translate(self, term):
+        # Retourne la traduction du terme dans la langue choisie
+        return self.SCORE_NAMES[term][self.lang]
 
     def won_point(self, playerName):
         if playerName == "player1":
@@ -25,26 +38,39 @@ class TennisGame6:
 
     def _tie_score(self):
         if self.player1Score < 3:
-            return TennisGame6.SCORE_NAMES[self.player1Score] + "-All"
+            return self.translate(self._score_to_string(self.player1Score)) + "-" + self.translate("All")
         else:
-            return "Deuce"
+            return self.translate("Deuce")
 
     def _end_game_score(self):
         score_diff = self.player1Score - self.player2Score
         if abs(score_diff) == 1:
             leader = self.player1Name if score_diff == 1 else self.player2Name
-            return "Advantage " + leader
+            return self.translate("Advantage") + leader
         else:
             winner = self.player1Name if score_diff >= 2 else self.player2Name
-            return "Win for " + winner
+            return self.translate("Win for") + winner
 
     def _regular_score(self):
-        return TennisGame6.SCORE_NAMES[self.player1Score] + "-" + TennisGame6.SCORE_NAMES[self.player2Score]
+       return self.translate(self._score_to_string(self.player1Score)) + "-" + self.translate(self._score_to_string(self.player2Score))
+
+    def winning_player(self):
+        if self.player1Score >= 4 and self.player1Score - self.player2Score >= 2:
+            return self.player1Name
+        elif self.player2Score >= 4 and self.player2Score - self.player1Score >= 2:
+            return self.player2Name
+        else:
+            return None
+
+    @staticmethod
+    def _score_to_string(score):
+        if score == 0:
+            return "Love"
+        elif score == 1:
+            return "Fifteen"
+        elif score == 2:
+            return "Thirty"
+        elif score == 3:
+            return "Forty"
 
 
-tenis = TennisGame6("toto","tata")
-tenis.won_point("titi")
-tenis.won_point("toto")
-tenis.won_point("toto")
-
-print(tenis.score())
